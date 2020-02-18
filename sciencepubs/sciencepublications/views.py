ab646dfdd01499f75c83052ab63a58c08c977a18
Sebastian Śmigiel <smigiel.sebastian@gmail.com>
2020-02-18 20:15:59 +0100

    added second method in which I send request to API

--- sciencepubs/sciencepublications/views.py
+++ sciencepubs/sciencepublications/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+import requests
+import json
 
 # Create your views here.
 def index(request):
     template = loader.get_template('publications/index.html')
     return HttpResponse(template.render())
+
+def sendRequest(request):
+    URL = "api.elsevier.com/content/search/scopus"
+    PARAMS = {
+        'query': 'all(gene)',
+        'apiKey': '15cc00bb996e844d8f6c871451bbd069'
+    }
+    response = requests.get('https://api.elsevier.com/content/search/scopus?query=all(gene)&apiKey=15cc00bb996e844d8f6c871451bbd069')
+    return HttpResponse(response)
