7868a3555300b98e2623f59756d0879f29cc309c
Tadzik <mtadzik@qg316313.qg.com>
2020-02-19 18:42:50 +0100

    Adding dynamic path for templates

--- sciencepubs/sciencepubs/settings.py
+++ sciencepubs/sciencepubs/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'sciencepubs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':[],
+        'DIRS':[os.path.join(BASE_DIR, "sciencepublications/templates/publications")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
